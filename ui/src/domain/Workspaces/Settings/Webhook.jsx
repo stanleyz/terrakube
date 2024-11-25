ad9cb606df66e90edf4c659ba718dbac2709953e
Stanley Zhang <stanley.zhang@ityin.net>
2024-11-25 23:32:28 +1300

    update UI

---
+++ ui/src/domain/Workspaces/Settings/Webhook.jsx
@@ -0,0 +1,92 @@
+import {
+    Table,
+    Space,
+    Popconfirm,
+} from "antd";
+
+export const WorkspaceWebhook = ({workspace, manageWorkspace}) => {
+    const onDelete = (id) => {
+    }
+    const columns = [
+        {
+            title: "Event",
+            dataIndex: "event",
+            key: "event",
+        },
+        {
+            title: "Branch",
+            dataIndex: "branch",
+            key: "branch",
+        },
+        {
+            title: "Folder",
+            dataIndex: "folder",
+            key: "folder",
+        },
+        {
+            title: "Template",
+            dataIndex: "template",
+            key: "template",
+        },
+        {
+            title: "Action",
+            key: "action",
+            render: (text, record) => (
+                <Space size="middle">
+                    <Popconfirm
+                        onConfirm={() => {
+                            onDelete(record.id);
+                        }}
+                        style={{ width: "20px" }}
+                        title={
+                            <p>
+                                This will permanently delete this trigger from the webhook<br />
+                                Are you sure?
+                            </p>
+                        }
+                        okText="Yes"
+                        cancelText="No"
+                        disabled={!manageWorkspace}
+                    >
+                        <a>Delete</a>
+                    </Popconfirm>
+                </Space>
+            ),
+        }
+    ] 
+    
+    const data = [
+        {
+            key: '1',
+            event: 'push',
+            branch: 'master',
+            folder: 'root',
+            template: 'Terraform',
+        },
+        {
+            key: '2',
+            event: 'push',
+            branch: 'master',
+            folder: 'root',
+            template: 'Terraform',
+        },
+        {
+            key: '3',
+            event: 'push',
+            branch: 'master',
+            folder: 'root',
+            template: 'Terraform',
+        },
+    ];
+    
+    return (
+        <div>
+            <h1>Webhook</h1>
+            <div className="App-Text">
+                Webhooks allow you to trigger a workspace run when a specific event occurs in the repository.
+            </div>
+            {/* <Table columns={columns} dataSource={workspace.webhooks} /> */}
+            <Table columns={columns} dataSource={data} />
+        </div>
+    );
+}
